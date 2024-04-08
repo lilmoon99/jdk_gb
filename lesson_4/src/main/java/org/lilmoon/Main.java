33640df291e5728674d44fade5bdbb2213cb21c2
lilmoon99 <oxygen4ik@bk.ru>
2024-04-08 14:47:05 +0400

    seminar 4 done

--- lesson_4/src/main/java/org/lilmoon/Main.java
+++ lesson_4/src/main/java/org/lilmoon/Main.java
@@ -1,17 +1,61 @@
 package org.lilmoon;
 
+/*
+1. Создайте коллекцию мужских и женских имен с помощью интерфейса List
+2. Отсортируйте коллекцию в алфавитном порядке
+3. Отсортируйте коллекцию по количеству букв в слове
+4. Разверните коллекцию
+ */
+
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> names = new ArrayList<>(initNames());
+        System.out.println("Our List");
+        System.out.println(names);
+//        System.out.println("Sorted list");
+//        Collections.sort(names);
+//        System.out.println(names);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        names.sort((String::compareTo));
+//        System.out.println(names);
+
+//        Collections.sort(names,(o1, o2) -> o2.length() - o1.length());
+//        System.out.println(names);
+
+//        Collections.reverse(names);
+//        System.out.println(names);
+
+//        reverseList(names);
+
+        Set<String> stringSet = new HashSet<>(names);
+//        for (String s : stringSet) {
+//            System.out.print(s + " ");
+//        }
+//
+//        System.out.println(stringSet.stream().min(String::compareTo).get());
+
+
+        System.out.println(Collections.max(stringSet,(o1, o2) -> o2.length() - o1.length()));
+        stringSet.removeIf(s -> s.contains("A"));
+        stringSet.removeIf(s -> s.contains("a"));
+        System.out.println(stringSet);
+
+    }
+
+    private static void reverseList(List<String> list) {
+        System.out.println("Reverse list");
+        ArrayList<String> temp = new ArrayList<>(list.size());
+        for (int i = list.size() - 1; i >=0; i--) {
+            temp.add(list.get(i));
         }
+        System.out.println(temp);
+    }
+
+    private static List<String> initNames(){
+        return Arrays.asList("Ivan","Maria","Aynur","Elisa","Mark","Elon","Aynur","Elisa");
     }
 }
--- lesson_4/src/main/java/org/lilmoon/Task3.java
+++ lesson_4/src/main/java/org/lilmoon/Task3.java
@@ -1,2 +1,40 @@
-package org.lilmoon;public class Task3 {
+package org.lilmoon;
+
+import java.util.*;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Map<String, String> phoneBook = new HashMap<>();
+        phoneBook.put("911", "Emergency");
+        phoneBook.put("311", "Fire fighters");
+        phoneBook.put("89911944602", "Aynur");
+        phoneBook.put("222","Police");
+        phoneBook.put("444","Gas service");
+
+
+
+
+
+
+        Set<String> keySet = phoneBook.keySet();
+        String minPhoneNumber = Collections.min(keySet, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() - o2.length() == 0) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+//        System.out.println(phoneBook.get(minPhoneNumber));
+
+        Collection<String> values = phoneBook.values();
+        System.out.println(values);
+        String maxValue = String.valueOf(values.stream().max((o1, o2) -> o1.length() - o2.length()).get());
+        for (Map.Entry<String, String> stringStringEntry : phoneBook.entrySet()) {
+            if (stringStringEntry.getValue().equals(maxValue)){
+                System.out.println(stringStringEntry);
+            }
+        }
+        System.out.println(maxValue);
+    }
 }
